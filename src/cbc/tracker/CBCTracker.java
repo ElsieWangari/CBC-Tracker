e7ecfc96c2997c53ea48879535fff551b2d62d52
Wangari Elsie <Wangari Elsie@Elsie>
2024-10-29 08:48:29 +0300

    First commit

---
+++ src/cbc/tracker/CBCTracker.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cbc.tracker;
+
+/**
+ *
+ * @author Wangari Elsie
+ */
+public class CBCTracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
